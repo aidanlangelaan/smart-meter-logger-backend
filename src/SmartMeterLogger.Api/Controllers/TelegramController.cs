575efad7a454c68222f7ef48330aba5e9375665a
Aidan Langelaan <aidanlangelaan@gmail.com>
2023-07-09 23:44:04 +0200

    More refactoring + upgrade to .net 7

---
+++ src/SmartMeterLogger.Api/Controllers/TelegramController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SmartMeterLogger.Api.Models;
+using SmartMeterLogger.Business.Interfaces;
+using SmartMeterLogger.Business.Models;
+
+namespace SmartMeterLogger.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelegramController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ITelegramService _telegramService;
+
+        public TelegramController(IMapper mapper, ITelegramService telegramService)
+        {
+            _mapper = mapper;
+            _telegramService = telegramService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTelegramViewModel model)
+        {
+            var createTelegramDto = _mapper.Map<CreateTelegramDTO>(model);
+            var result = await _telegramService.Create(createTelegramDto);
+            
+            var getTelegramViewModel = _mapper.Map<GetTelegramViewModel>(result);
+            return Created(getTelegramViewModel.Id.ToString(), getTelegramViewModel);
+        }
+    }
+}
---
+++ src/SmartMeterLogger.Data/Entities/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartMeterLogger.Data.Entities
+{
+    public class AuditableEntity : EntityBase
+    {
+        [Required]
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedOnAt { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime")]
+        public DateTime UpdatedOnAt { get; set; }
+    }
+}
