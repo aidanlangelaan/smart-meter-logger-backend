6526c0fed0b9ce3da2a33421ab8a30e2cde72104
Aidan Langelaan <aidanlangelaan@gmail.com>
2023-10-29 15:16:36 +0100

    Temporarily disabled fluent validation rules for serial number due to validation not working as expected

--- src/SmartMeterLogger.Api/Models/ElectricityUsage/Validators/GetElectricityUsageRequestViewModelValidator.cs
+++ src/SmartMeterLogger.Api/Models/ElectricityUsage/Validators/GetElectricityUsageRequestViewModelValidator.cs
@@ -7,9 +7,9 @@ public class GetElectricityUsageRequestViewModelValidator : AbstractValidator<Ge
     public GetElectricityUsageRequestViewModelValidator()
     {
         RuleFor(model => model.SerialNumber)
-            .NotEmpty()
-            .Length(34)
-            .Matches("^[0-9]*$");
+            .NotEmpty();
+            // .Length(34)
+            // .Matches("^[0-9]*$");
         
         RuleFor(model => model.pageSize).GreaterThan(0);
     }
